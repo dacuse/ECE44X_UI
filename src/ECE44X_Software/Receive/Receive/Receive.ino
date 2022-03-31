504043639169a8944f50263e9aa849c090bb4c7a
Bryson Goto <gotob@oregonstate.edu>
2022-03-30 20:19:53 -0700

    Removed old files, added Feather script with sensor and LoRa implementation

--- src/ECE44X_Software/Receive/Receive/Receive.ino
+++ src/ECE44X_Software/Receive/Receive/Receive.ino
@@ -1,42 +1,9 @@
-// Feather9x_RX
-// -*- mode: C++ -*-
-// Example sketch showing how to create a simple messaging client (receiver)
-// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
-// reliability, so you should only use RH_RF95 if you do not need the higher
-// level messaging abilities.
-// It is designed to work with the other example Feather9x_TX
-
 #include <SPI.h>
 #include <RH_RF95.h>
 
-/* for Feather32u4 RFM9x
-#define RFM95_CS 8
-#define RFM95_RST 4
-#define RFM95_INT 7
-*/
-
-// for feather m0 RFM9x
 #define RFM95_CS 8
 #define RFM95_RST 4
 #define RFM95_INT 3
-
-/* for shield 
-#define RFM95_CS 10
-#define RFM95_RST 9
-#define RFM95_INT 7
-*/
-
-/* Feather 32u4 w/wing
-#define RFM95_RST     11   // "A"
-#define RFM95_CS      10   // "B"
-#define RFM95_INT     2    // "SDA" (only SDA/SCL/RX/TX have IRQ!)
-*/
-
-/* Feather m0 w/wing 
-#define RFM95_RST     11   // "A"
-#define RFM95_CS      10   // "B"
-#define RFM95_INT     6    // "D"
-*/
 
 #if defined(ESP8266)
   /* for ESP w/featherwing */ 
@@ -44,19 +11,24 @@
   #define RFM95_RST 16   // "D"
   #define RFM95_INT 15   // "B"
 
-#elif defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S2)
+#elif defined(ADAFRUIT_FEATHER_M0) || defined(ADAFRUIT_FEATHER_M0_EXPRESS) || defined(ARDUINO_SAMD_FEATHER_M0)
+  // Feather M0 w/Radio
+  #define RFM95_CS      8
+  #define RFM95_INT     3
+  #define RFM95_RST     4
+
+#elif defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S2) || defined(ARDUINO_NRF52840_FEATHER) || defined(ARDUINO_NRF52840_FEATHER_SENSE)
   #define RFM95_INT     9  // "A"
   #define RFM95_CS      10  // "B"
   #define RFM95_RST     11  // "C"
-  #define LED       
-
+  
 #elif defined(ESP32)  
   /* ESP32 feather w/wing */
   #define RFM95_RST     27   // "A"
   #define RFM95_CS      33   // "B"
   #define RFM95_INT     12   //  next to A
 
-#elif defined(NRF52)  
+#elif defined(ARDUINO_NRF52832_FEATHER)
   /* nRF52832 feather w/wing */
   #define RFM95_RST     7   // "A"
   #define RFM95_CS      11   // "B"
@@ -69,29 +41,16 @@
   #define RFM95_INT     4    // "C"
 #endif
 
-
 // Change to 434.0 or other frequency, must match RX's freq!
 #define RF95_FREQ 915.0
 
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
-// Blinky on receipt
-#define LED 13
-
-void setup()
-{
-  pinMode(LED, OUTPUT);
+void setup() {
+  
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
-
-  Serial.begin(115200);
-  while (!Serial) {
-    delay(1);
-  }
-  delay(100);
-
-  Serial.println("Feather LoRa RX Test!");
 
   // manual reset
   digitalWrite(RFM95_RST, LOW);
@@ -119,10 +78,11 @@ void setup()
   // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
   // you can set transmitter powers from 5 to 23 dBm:
   rf95.setTxPower(23, false);
+
 }
 
-void loop()
-{
+void loop() {
+  
   if (rf95.available())
   {
     // Should be a message for us now
@@ -131,23 +91,16 @@ void loop()
 
     if (rf95.recv(buf, &len))
     {
-      digitalWrite(LED, HIGH);
       RH_RF95::printBuffer("Received: ", buf, len);
       Serial.print("Got: ");
       Serial.println((char*)buf);
-       Serial.print("RSSI: ");
+      Serial.print("RSSI: ");
       Serial.println(rf95.lastRssi(), DEC);
-
-      // Send a reply
-      uint8_t data[] = "And hello back to you";
-      rf95.send(data, sizeof(data));
-      rf95.waitPacketSent();
-      Serial.println("Sent a reply");
-      digitalWrite(LED, LOW);
     }
     else
     {
       Serial.println("Receive failed");
     }
   }
+
 }
---
+++ src/ECE44X_Software/Transmit/Transmit.ino
@@ -0,0 +1,366 @@
+#include <Arduino.h>
+
+// Teros libraries
+#include "SDI12.h"
+
+// BME libraries
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+// SD libraries
+#include <SD.h>
+#include <SPI.h>
+
+// LoRa libraries
+#include <RH_RF95.h>
+
+// Data manipulation
+#include <string>
+#include <cstring>
+
+// Teros data wire
+#define DATAPIN 11         // change to the proper pin
+
+// LoRa pins
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
+
+SDI12 mySDI12(DATAPIN);
+
+int packet = 1;
+float dielectric, temp, humid;
+
+Adafruit_BME280 bme; // I2C
+File myFile;
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+// keeps track of active addresses
+// each bit represents an address:
+// 1 is active (taken), 0 is inactive (available)
+// setTaken('A') will set the proper bit for sensor 'A'
+byte addressRegister[8] = {
+  0B00000000,
+  0B00000000,
+  0B00000000,
+  0B00000000,
+  0B00000000,
+  0B00000000,
+  0B00000000,
+  0B00000000
+};
+
+
+// converts allowable address characters '0'-'9', 'a'-'z', 'A'-'Z',
+// to a decimal number between 0 and 61 (inclusive) to cover the 62 possible addresses
+byte charToDec(char i){
+  if((i >= '0') && (i <= '9')) return i - '0';
+  if((i >= 'a') && (i <= 'z')) return i - 'a' + 10;
+  if((i >= 'A') && (i <= 'Z')) return i - 'A' + 37;
+  else return i;
+}
+
+// gets identification information from a sensor, and prints it to the serial port
+// expects a character between '0'-'9', 'a'-'z', or 'A'-'Z'.
+void printInfo(char i){
+  int j;
+  String command = "";
+  command += (char) i;
+  command += "I!";
+  for(j = 0; j < 1; j++){
+    mySDI12.sendCommand(command);
+    delay(30);
+    if(mySDI12.available()>1) break;
+    if(mySDI12.available()) mySDI12.read();
+  }
+
+  while(mySDI12.available()){
+    char c = mySDI12.read();
+    if((c!='\n') && (c!='\r')) Serial.write(c);
+    delay(5);
+  }
+}
+
+void printBufferToScreen(){
+  String buffer = "";
+  mySDI12.read(); // consume address
+  mySDI12.read(); // consume address
+  while(mySDI12.available()){
+    char c = mySDI12.read();
+    if(c == '+' || c == '-'){
+      buffer += ',';
+      if(c == '-') buffer += '-';
+    }
+    else if ((c != '\n') && (c != '\r')) {
+      buffer += c;
+    }
+    delay(50);
+  }
+ char* val = strdup(buffer.c_str());
+ dielectric = pow(2.887*pow(10, -9)*pow(atof(val), 3)-2.080*pow(10, -5)*pow(atof(val),2)+5.276*pow(10,-2)*atof(val)-43.39, 2);
+ Serial.print("Dielectric: ");
+ Serial.println(dielectric);
+}
+
+void takeMeasurement(char i){
+  String command = "";
+  command += i;
+  command += "M!"; // SDI-12 measurement command format  [address]['M'][!]
+  mySDI12.sendCommand(command);
+  // wait for acknowlegement with format [address][ttt (3 char, seconds)][number of measurments available, 0-9]
+  String sdiResponse = "";
+  delay(30);
+  while (mySDI12.available())  // build response string
+  {
+    char c = mySDI12.read();
+    if ((c != '\n') && (c != '\r'))
+    {
+      sdiResponse += c;
+      delay(5);
+    }
+  }
+  mySDI12.clearBuffer();
+
+  // find out how long we have to wait (in seconds).
+  unsigned int wait = 0;
+  wait = sdiResponse.substring(1,4).toInt();
+
+  // Set up the number of results to expect
+  // int numMeasurements =  sdiResponse.substring(4,5).toInt();
+
+  unsigned long timerStart = millis();
+  while((millis() - timerStart) < (1000 * wait)){
+    if(mySDI12.available())  // sensor can interrupt us to let us know it is done early
+    {
+      mySDI12.clearBuffer();
+      break;
+    }
+  }
+  // Wait for anything else and clear it out
+  delay(30);
+  mySDI12.clearBuffer();
+
+  // in this example we will only take the 'DO' measurement
+  command = "";
+  command += i;
+  command += "D0!"; // SDI-12 command to get data [address][D][dataOption][!]
+  mySDI12.sendCommand(command);
+  while(!mySDI12.available()>1); // wait for acknowlegement
+  delay(300); // let the data transfer
+  printBufferToScreen();
+  mySDI12.clearBuffer();
+}
+
+// this checks for activity at a particular address
+// expects a char, '0'-'9', 'a'-'z', or 'A'-'Z'
+boolean checkActive(char i){
+
+  String myCommand = "";
+  myCommand = "";
+  myCommand += (char) i;                 // sends basic 'acknowledge' command [address][!]
+  myCommand += "!";
+
+  for(int j = 0; j < 3; j++){            // goes through three rapid contact attempts
+    mySDI12.sendCommand(myCommand);
+    if(mySDI12.available()>1) break;
+    delay(30);
+  }
+  if(mySDI12.available()>2){       // if it hears anything it assumes the address is occupied
+    mySDI12.clearBuffer();
+    return true;
+  }
+  else {   // otherwise it is vacant.
+    mySDI12.clearBuffer();
+  }
+  return false;
+}
+
+// this quickly checks if the address has already been taken by an active sensor
+boolean isTaken(byte i){
+  i = charToDec(i); // e.g. convert '0' to 0, 'a' to 10, 'Z' to 61.
+  byte j = i / 8;   // byte #
+  byte k = i % 8;   // bit #
+  return addressRegister[j] & (1<<k); // return bit status
+}
+
+// this sets the bit in the proper location within the addressRegister
+// to record that the sensor is active and the address is taken.
+boolean setTaken(byte i){
+  boolean initStatus = isTaken(i);
+  i = charToDec(i); // e.g. convert '0' to 0, 'a' to 10, 'Z' to 61.
+  byte j = i / 8;   // byte #
+  byte k = i % 8;   // bit #
+  addressRegister[j] |= (1 << k);
+  return !initStatus; // return false if already taken
+}
+
+// BME280 measurement function
+void bme_measure() {
+
+    bme.begin(0x76);
+
+    temp = bme.readTemperature();
+    humid = bme.readHumidity();
+ 
+    Serial.print("Temp: ");
+    Serial.print(temp);
+    Serial.println(" C");
+
+    Serial.print("Humidity: ");
+    Serial.print(humid);
+    Serial.println(" %");
+  
+}
+
+void setup(){
+  
+  Serial.begin(9600);
+
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  pinMode(5, OUTPUT);
+  digitalWrite(5, LOW); // Sets pin 5, the pin with the 3.3V rail, to output and enables the rail
+  pinMode(6, OUTPUT);
+  digitalWrite(6, HIGH); // Sets pin 6, the pin with the 5V rail, to output and enables the rail
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+
+  mySDI12.begin();
+  delay(500); // allow things to settle
+
+  Serial.println("Scanning all addresses, please wait...");
+  /*
+      Quickly Scan the Address Space
+   */
+
+  for(byte i = '0'; i <= '9'; i++) if(checkActive(i)) setTaken(i);   // scan address space 0-9
+
+  for(byte i = 'a'; i <= 'z'; i++) if(checkActive(i)) setTaken(i);   // scan address space a-z
+
+  for(byte i = 'A'; i <= 'Z'; i++) if(checkActive(i)) setTaken(i);   // scan address space A-Z
+
+  /*
+      See if there are any active sensors.
+   */
+  boolean found = false;
+
+  for(byte i = 0; i < 62; i++){
+    if(isTaken(i)){
+      found = true;
+      Serial.print("Found address:  ");
+      Serial.println(i);
+      break;
+    }
+  }
+}
+
+void sd_log() {
+
+    SD.begin(10);
+
+    if (packet == 1) {
+        if (!SD.begin(10)) {
+            Serial.println("SD failed to initialize");
+            return;
+        }
+
+        myFile = SD.open("test.csv", FILE_WRITE);
+
+        if (myFile) {
+            myFile.println("Packet,Temperature,Humidity,Dielectric");
+            myFile.close();
+            Serial.println("SD initialized");
+        }
+    }
+
+    myFile = SD.open("test.csv", FILE_WRITE);
+
+    if (myFile) {
+      // Packet Number
+      myFile.print(packet);
+      myFile.print(",");
+
+      // Temperature
+      myFile.print(temp);
+      myFile.print(",");
+
+      // Humidity
+      myFile.print(humid);
+      myFile.print(",");
+
+      // Electrical Conductivity 
+      myFile.print(dielectric);
+      myFile.println(",");
+
+      Serial.println("Logged to SD");
+      myFile.close();
+      return;
+    }
+    Serial.println("Log failed");
+}
+
+void loop(){
+
+  // scan address space 0-9
+  for(char i = '0'; i <= '9'; i++) if(isTaken(i)){
+    takeMeasurement(i);
+  }
+
+  // scan address space a-z
+  for(char i = 'a'; i <= 'z'; i++) if(isTaken(i)){
+    takeMeasurement(i);
+  }
+
+  // scan address space A-Z
+  for(char i = 'A'; i <= 'Z'; i++) if(isTaken(i)){
+    takeMeasurement(i);
+  };
+
+  bme_measure();
+  sd_log();
+
+  String temp_link = "https://web.engr.oregonstate.edu/~eaglea/add_data.php?temp=" + String(temp) + "&hum=" + String(humid) + "&pr=" + String(dielectric);
+  int link_len = temp_link.length() + 1;
+  char converted_value[link_len];
+  temp_link.toCharArray(converted_value, link_len);
+
+  Serial.println(converted_value);
+
+  rf95.send((uint8_t *)converted_value, link_len);
+  delay(10);
+  rf95.waitPacketSent();
+  
+  delay(5000); // wait ten seconds between measurement attempts.
+}
